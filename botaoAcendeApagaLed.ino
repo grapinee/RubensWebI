429f94010900e40665c9f3d79ef6ad75677108f1
grapinee <45693781+grapinee@users.noreply.github.com>
2026-09-18 16:55:05 -0300

    ledacendeapaga
    jjdhd

---
+++ botaoAcendeApagaLed.ino
@@ -0,0 +1,47 @@
+// C++ code
+//
+#define led 4
+#define btn 3
+int btnclicado = 0;
+int btnliberado = 0;
+
+void trocaLed(){
+  if (digitalRead(led)== HIGH){
+  digitalWrite(led, LOW);
+  }else{
+  digitalWrite(led, HIGH);
+  }
+}
+void Keyup(){
+  if((btnclicado == 1)and (btnliberado == 1)){
+      btnclicado = 0;
+  	  btnliberado = 0;
+  	  trocaLed();
+  }
+}
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+   pinMode(btn, INPUT);
+}
+
+void loop()
+{
+  if(digitalRead(btn)== HIGH){
+    btnclicado = 1;
+    btnliberado = 0;
+ //   digitalWrite(led == high);
+  
+  }else{
+    btnliberado = 1;
+   // ditalWrite (led. low);
+  }
+Keyup();
+}
+ 
+
+
+
+
+
